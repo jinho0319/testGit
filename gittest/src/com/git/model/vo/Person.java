7335cd821ad0d13fda5fe6c680b1edb17821717b
jinho0319 <ckrka@naver.com>
2020-09-24 21:24:14 +0900

    #06 Person 전화번호 이메일 추가 -JH-

--- gittest/src/com/git/model/vo/Person.java
+++ gittest/src/com/git/model/vo/Person.java
@@ -7,6 +7,8 @@ public class Person {
 	private double weight;
 	private String[] hobby;
 	private String address;
+	private String phone;
+	private String email;
 
 	public Person() {
 		// TODO Auto-generated constructor stub
