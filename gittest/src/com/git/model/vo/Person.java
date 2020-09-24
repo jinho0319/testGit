451a1b0b286a0c2345b37e9d41c91de3c9a08827
jinho0319 <ckrka@naver.com>
2020-09-24 21:07:28 +0900

    #01 Person 키,몸무게 변수, 생성자 추가 -rclass-

--- gittest/src/com/git/model/vo/Person.java
+++ gittest/src/com/git/model/vo/Person.java
@@ -3,5 +3,10 @@ package com.git.model.vo;
 public class Person {
 	private String name;
 	private int age;
-
+	private double height;
+	private double weight;
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
 }
